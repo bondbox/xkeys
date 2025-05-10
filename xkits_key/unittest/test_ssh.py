3237aeb7789178b1b7164feb800083533f0d9b5e
Mingzhe Zou <zoumingzhe@outlook.com>
2025-05-11 00:18:58 +0800

    ssh: implement SSHKeys class

--- xkits_key/unittest/test_ssh.py
+++ xkits_key/unittest/test_ssh.py
@@ -7,7 +7,7 @@ import unittest
 from xkits_key import ssh
 
 
-class TestSSH(unittest.TestCase):
+class TestSSHKeys(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
@@ -23,32 +23,39 @@ class TestSSH(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_generate(self):
+    def test_keygen(self):
         with TemporaryDirectory() as temp:
             chdir(temp)
-            key, pub = ssh.generate()
+            key, pub = ssh.SSHKeys.keygen()
             self.assertIsInstance(key, str)
             self.assertIsInstance(pub, str)
-            key, pub = ssh.generate()
+            key, pub = ssh.SSHKeys.keygen()
             self.assertIsInstance(key, str)
             self.assertIsInstance(pub, str)
 
     def test_generate_dir(self):
         with TemporaryDirectory() as temp:
-            key, pub = ssh.generate(keyfile=temp)
+            keys = ssh.SSHKeys(temp)
+            self.assertEqual(len(keys), 0)
+            key, pub = keys.generate(keyfile=temp)
             self.assertIsInstance(key, str)
             self.assertIsInstance(pub, str)
-            key, pub = ssh.generate(keyfile=temp)
+            self.assertEqual(len(keys), 1)
+            key, pub = keys.generate(keyfile=temp)
             self.assertIsInstance(key, str)
             self.assertIsInstance(pub, str)
+            self.assertEqual(len(keys), 2)
 
     def test_generate_file(self):
         with TemporaryDirectory() as temp:
-            file: str = ssh.join(temp, "unittest")
-            key, pub = ssh.generate(keyfile=file)
-            self.assertEqual(key, file)
-            self.assertEqual(pub, f"{file}.pub")
-            self.assertRaises(FileExistsError, ssh.generate, keyfile=file)
+            keys = ssh.SSHKeys(temp)
+            self.assertEqual(len(keys), 0)
+            key, pub = keys.generate(keyfile="unittest")
+            self.assertEqual(len(keys), 1)
+            self.assertEqual(key, ssh.join(keys.base, "unittest"))
+            self.assertEqual(pub, ssh.join(keys.base, "unittest.pub"))
+            self.assertRaises(FileExistsError, keys.generate, keyfile="unittest")  # noqa:E501
+            self.assertEqual(len(keys), 1)
 
 
 if __name__ == "__main__":
