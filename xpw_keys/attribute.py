a422b5e55fb01f183a3d49a3cfe7e87c2c447787
Mingzhe Zou <zoumingzhe@qq.com>
2025-05-28 10:31:32 +0800

    bump version to 0.5.0

--- xpw_keys/attribute.py
+++ xpw_keys/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xkeys"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __urlhome__ = "https://github.com/bondbox/xkeys/"
 __description__ = "Key and Certificate Management"
 
